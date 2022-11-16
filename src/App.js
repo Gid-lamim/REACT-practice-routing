79f4625003033309a88a4fab709c42e824dc7db1
Gid.Lamim <gid.lamim@gmail.com>
2022-11-16 16:21:21 -0300

    add Routes to app.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,27 @@ import Welcome from './components/Welcome'
 import Products from './components/Products'
 
 function App() {
+  //we use Route as a normal component.
+  /*Using Route will allow us to load different pages according to the path we choose.
+    our-domain.com/welcome  will load the welcome page for example.
+  */
+
+  
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Route path="/welcome">
+        <Welcome/>
+      </Route>
+      <Route path="/products">
+        <Products/>
+      </Route>
+      <p>Here we have two paths: welcome and products. <br/> It doesn't render a different page. <br/>It just renders the component into this existing page according to the chosen path</p>
+      <a href="/welcome">welcome</a>
+      <br/>
+      <a href="/products">products</a>
     </div>
+    
   );
 }
 
