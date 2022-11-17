bae79b0855595cac9c154ba5b9dc724a134745f7
Gid.Lamim <gid.lamim@gmail.com>
2022-11-17 18:04:38 -0300

    add dynamicroute to display product details
    Sometimes we need to add a path that access the same page but we need to pass props. Dynamic route will help achieving that.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import {Route} from 'react-router-dom';
 
+import MainHeader from './components/MainHeader';
 import Welcome from './pages/Welcome'
 import Products from './pages/Products'
-import MainHeader from './components/MainHeader';
+import ProductDetail from './pages/ProductDetail';
 
 function App() {
   //we use Route as a normal component.
@@ -10,6 +11,9 @@ function App() {
     our-domain.com/welcome  will load the welcome page for example.
   */
 
+  /*Using dynamic routes is done using colons.
+    our-domain.com/product-details/<idHere>  will load the welcome page for example.
+  */
   
 
   return (
@@ -22,6 +26,10 @@ function App() {
         <Route path="/products">
           <Products/>
         </Route>
+        <Route path="/product-detail/:productId">
+          <ProductDetail/>
+        </Route>
+
         <p>Here we have two paths: welcome and products. <br/> It doesn't render a different page. <br/>It just renders the component into this existing page according to the chosen path</p>
         
       </main>
