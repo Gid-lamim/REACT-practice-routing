39f8313f5c3e50adffa289d0dee65d2dc2b8e7dc
Gid.Lamim <gid.lamim@gmail.com>
2022-11-17 17:54:32 -0300

    create ProductDetail page

---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,9 @@
+const ProductDetail = () =>{
+    return (
+        <section>
+            <h2>Product Details here</h2>
+        </section>
+    );
+}
+
+export default ProductDetail;
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -4,15 +4,9 @@ const Products = () => {
                 <h1>This is the Products page</h1>
 
                 <ul>
-                    <li>
-                        Product 1
-                    </li>
-                    <li>
-                        Product 2
-                    </li>
-                    <li>
-                        Product 3
-                    </li>
+                    <li>Product 1</li>
+                    <li>Product 2</li>
+                    <li>Product 3</li>
                 </ul>
         </section>
     );
