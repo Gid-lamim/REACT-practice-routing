4505e32258b9095893c3ad807fe098b732dc57b8
Gid.Lamim <gid.lamim@gmail.com>
2022-11-18 13:18:20 -0300

    Add link to product details
    Now we can use the Link hook because we don't need to show which link is active

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
     return( 
         <section>
                 <h1>This is the Products page</h1>
 
                 <ul>
-                    <li>old Books</li>
-                    <li>Old screen door</li>
-                    <li>Used toilet paper</li>
+                    <li><Link to='/products/p1'>old Books</Link></li>
+                    <li><Link to='/products/p2'>Old screen door</Link></li>
+                    <li><Link to='/products/p3'>Used toilet paper</Link></li>
                 </ul>
         </section>
     );
